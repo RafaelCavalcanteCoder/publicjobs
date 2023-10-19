60fef6789f0d9e6254b2f4b601f12040f2ede0b7
Rafael <rafa@gmail.com>
2023-10-18 21:45:56 -0300

    Início do Projeto

---
+++ src/main/java/br/com/publicjobs/blogs/PublicJobsBlogsApplication.java
@@ -0,0 +1,13 @@
+package br.com.publicjobs.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicJobsBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicJobsBlogsApplication.class, args);
+	}
+
+}
