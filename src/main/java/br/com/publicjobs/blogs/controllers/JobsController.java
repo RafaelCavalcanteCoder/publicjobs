8201d90974cf8d5ce0affe1fed0e34c30dacfdd6
Rafael <rafa@gmail.com>
2023-10-18 23:59:46 -0300

    Inicio Metodo Post

--- src/main/java/br/com/publicjobs/blogs/controllers/JobsController.java
+++ src/main/java/br/com/publicjobs/blogs/controllers/JobsController.java
@@ -1,5 +1,16 @@
 package br.com.publicjobs.blogs.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postar")
 public class JobsController {
+
+    @PostMapping
+    public void cadastrar(){
+
+    }
     
 }
